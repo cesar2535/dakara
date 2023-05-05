e2406bafe8189ffff93f10be2cd61fcff521905c
Cesar Chen <cesar6134@gmail.com>
2023-05-06 04:03:00 +0900

    feat(config): update config constants

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,12 @@
 import "./env";
 
-const config = {
-  COMPANY_CODE: process.env.COMPANY_CODE,
-  EMPLOYEE_CODE: process.env.EMPLOYEE_CODE,
-  PASSWORD: process.env.PASSWORD,
-};
+export const COMPANY_CODE = process.env.COMPANY_CODE;
+export const EMPLOYEE_CODE = process.env.EMPLOYEE_CODE;
+export const PASSWORD = process.env.PASSWORD;
 
-export default config;
+export const AUTH_BASE_URL = process.env.AUTH_BASE_URL;
+export const HRM_BASE_URL = process.env.HRM_BASE_URL;
+export const LINKUP_BASE_URL = process.env.LINKUP_BASE_URL;
+
+export const USER_AGENT = process.env.USER_AGENT;
+export const SRV_LOCATION = process.env.SRV_LOCATION;
